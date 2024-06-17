c7803c27db3250d71e3cf0295dde48d76d0bcbcd
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-06-17 15:46:30 +0300

    Fix problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskCalendarComponent } from './component/task-calendar/task-calendar.component';
-import { JulianPipe } from './pipes/calendar-pipe/julian.pipe';
 import { FormsModule } from '@angular/forms';
-import { BaikalPipe } from './pipes/baikal-pipe/baikal.pipe';
 import { HttpClientModule } from '@angular/common/http';
-import { GetApiService } from './component/get-api.service';
-import { ApiResponseComponent } from './component/api-response/api-response.component';
+
 
 @NgModule({
   declarations: [
-    ApiResponseComponent,
-    BaikalPipe,
-    JulianPipe,
     AppComponent,
-    TaskCalendarComponent,
   ],
   imports: [HttpClientModule, BrowserModule, AppRoutingModule, FormsModule],
-  providers: [GetApiService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
