d8171e8be760044c86ac85b05ccdc0f58495d7f2
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-06-17 15:43:04 +0300

    Delete not needed file, start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskCalendarComponent } from './component/task-calendar/task-calendar.component';
+import { JulianPipe } from './pipes/calendar-pipe/julian.pipe';
+import { FormsModule } from '@angular/forms';
+import { BaikalPipe } from './pipes/baikal-pipe/baikal.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { GetApiService } from './component/get-api.service';
+import { ApiResponseComponent } from './component/api-response/api-response.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ApiResponseComponent,
+    BaikalPipe,
+    JulianPipe,
+    AppComponent,
+    TaskCalendarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [HttpClientModule, BrowserModule, AppRoutingModule, FormsModule],
+  providers: [GetApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
