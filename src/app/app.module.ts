6aea5ce62bf2b9de9c37f352ab1099e208874179
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-06-17 20:53:19 +0300

    Add layout for 3 component (header, format-event, application-button)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './component/header/header.component';
+import {NgOptimizedImage} from "@angular/common";
+import { FormatEventComponent } from './component/format-event/format-event.component';
+import { ApplicationButtonComponent } from './component/application-button/application-button.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FormatEventComponent,
+    ApplicationButtonComponent,
   ],
-  imports: [HttpClientModule, BrowserModule, AppRoutingModule, FormsModule],
+  imports: [HttpClientModule, BrowserModule, AppRoutingModule, FormsModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/component/format-event/format-event.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-format-event',
+  templateUrl: './format-event.component.html',
+  styleUrls: ['./format-event.component.scss'],
+})
+export class FormatEventComponent {}
